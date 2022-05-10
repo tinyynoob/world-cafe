37b37413208a5eb9187868d4635bbffca99ec510
tinyynoob <sadpotato1920@gmail.com>
2022-05-11 01:02:57 +0800

    Add a structure to shuffle the output
    The intermediate results are stored to a .tmp file.
    After all the semi_sudoku are generated, we read it to a data structure
    wc. Then shuffle the players and output.

--- world_cafe.c
+++ world_cafe.c
@@ -8,6 +8,12 @@
 short **permu;
 int permu_size;
 
+static struct wc {  // world cafe
+    short **players;
+    int player_num;
+    short round;
+};
+
 void print_matrix(short **matrix, short round)
 {
     for (int i = 0; i < round; i++) {
@@ -25,8 +31,56 @@ static inline void swapint(int *a, int *b)
     *b = temp;
 }
 
+static struct wc *wc_init(short round, int player_num)
+{
+    struct wc *w = (struct wc *) malloc(sizeof(struct wc));
+    w->player_num = player_num;
+    w->players = (short **) malloc(sizeof(short *) * w->player_num);
+    w->round = round;
+    return w;
+}
+
+static inline void wc_swap_player(struct wc *w, int a, int b)
+{
+    short *temp = w->players[a];
+    w->players[a] = w->players[b];
+    w->players[b] = temp;
+}
+
+static void wc_shuffle_players(struct wc *w)
+{
+    for (int i = 0; i < w->player_num; i++) {
+        int random = ((unsigned) ((rand() << 16) | rand())) % w->player_num;
+        wc_swap_player(w, i, random);
+    }
+}
+
+static void wc_printout_players(struct wc *w, FILE *f)
+{
+    for (int i = 0; i < w->player_num; i++) {
+        fprintf(f, "player##%d,", i + 1);
+        for (int r = 0; r < w->round; r++)
+            fprintf(f, "%hd,", w->players[i][r] + 1);
+        putc('\n', f);
+    }
+}
+
+static void wc_printout_table(struct wc *w, FILE *f)
+{
+    // TODO
+}
+
+static inline void wc_destroy(struct wc *w)
+{
+    for (int i = 0; i < w->player_num; i++)
+        free(w->players[i]);
+    free(w->players);
+    free(w);
+}
+
 int main()
 {
+    srand(time(NULL));
     short round;
     printf("round (1 ~ 10): ");
     scanf("%hd", &round);
@@ -41,20 +95,14 @@ int main()
         printf("try again: ");
         scanf("%d", &player);
     }
-    srand(time(NULL));
     permu = permutation(round, &permu_size);
-    FILE *f = fopen("world_cafe.csv", "w");
+    FILE *f = fopen("world_cafe.tmp", "w");
     for (int count = 0; count < player + round;) {
         short **matrix = semi_sudoku(round);
-        int order[round];
-        for (int i = 0; i < round; i++)
-            order[i] = i;
-        for (int i = 0; i < round; i++)  // shuffle
-            swapint(&order[i], &order[rand() % round]);
         for (int i = 0; i < round; i++) {
-            fprintf(f, "name##%d,", ++count);
+            ++count;
             for (int j = 0; j < round; j++)
-                fprintf(f, "%hd,", matrix[order[i]][j] + 1);
+                fprintf(f, "%hd,", matrix[i][j]);
             putc('\n', f);
         }
         for (int i = 0; i < round; i++)
@@ -65,5 +113,22 @@ int main()
     for (int i = 0; i < permu_size; i++)
         free(permu[i]);
     free(permu);
+
+    f = fopen("world_cafe.tmp", "r");
+    struct wc *w = wc_init(round, player);
+    for (int i = 0; i < w->player_num; i++) {
+        w->players[i] = (short *) malloc(sizeof(short) * w->round);
+        for (int r = 0; r < w->round; r++)
+            fscanf(f, "%hd,", &w->players[i][r]);
+        fscanf(f, "\n");
+    }
+    fclose(f);
+    remove("world_cafe.tmp");
+    wc_shuffle_players(w);
+    f = fopen("world_cafe.csv", "w");
+    wc_printout_players(w, f);
+    fclose(f);
+    wc_destroy(w);
+    w = NULL;
     return 0;
 }
